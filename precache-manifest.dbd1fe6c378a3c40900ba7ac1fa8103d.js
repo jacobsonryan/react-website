6ef44c0a4e496f268768cc84e5376a334761b646
Ryan Jacobson <ryan@Ryans-MBP.home>
2020-06-29 19:17:06 -0400

    Updates

---
+++ precache-manifest.dbd1fe6c378a3c40900ba7ac1fa8103d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da8cacdec6aa96fb75a687eb0fbbc7d",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "279c66e9ce84924aa396",
+    "url": "/react-website/static/css/main.c4482a81.chunk.css"
+  },
+  {
+    "revision": "8250f4752854022faf31",
+    "url": "/react-website/static/js/2.347c2ed1.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/react-website/static/js/2.347c2ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "279c66e9ce84924aa396",
+    "url": "/react-website/static/js/main.9de8a4c9.chunk.js"
+  },
+  {
+    "revision": "d7197561124abc56adbb",
+    "url": "/react-website/static/js/runtime-main.3b5f61aa.js"
+  },
+  {
+    "revision": "9efdd0fc228f6941ff185c900deea883",
+    "url": "/react-website/static/media/Balto-Black.9efdd0fc.ttf"
+  },
+  {
+    "revision": "930f13595b53bd64c3a50577340c5cba",
+    "url": "/react-website/static/media/Balto-Medium.930f1359.ttf"
+  },
+  {
+    "revision": "7a3a4b15ab4e6b71aea71357d14ce144",
+    "url": "/react-website/static/media/HarrietDisplay-Black.7a3a4b15.ttf"
+  }
+]);
